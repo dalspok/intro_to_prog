b9c3a5ad801d23ebf410f42b2de4b8b314cda726
Dalibor Spok <dalspok@gmail.com>
2017-01-30 11:09:41 +0100

    basics

---
+++ basics/3.rb
@@ -0,0 +1,4 @@
+hsh = {"movie one" => 1975, "movie two" => 2001}
+
+puts hsh["movie one"]
+puts hsh["movie two"]
---
+++ basics/4.rb
@@ -0,0 +1,5 @@
+dates = [1965, 2001, 2013]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
