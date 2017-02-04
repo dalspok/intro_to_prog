d7ab2c91b16766b306544c1d9071dd7ce99b1711
Dalibor Spok <dalspok@gmail.com>
2017-02-04 11:21:10 +0100

    exerc

--- exercises/ex_14.rb
+++ exercises/ex_14.rb
@@ -1,19 +1,13 @@
 contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
             ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
-
 contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+fields = [:email, :address, :phone]
 
-
-keys = [:email, :address, :phone]
-
-contact_data.each_index do |j|
-
-  contact_data[j].each_index do |i|
-    contacts[contacts.keys[j]][keys[i]] = contact_data[j][i]
-    
+contacts.each do |key, hsh|
+  (1..3).each do |index|
+    hsh[fields[index]] = contact_data[index]
   end
 end
 
 p contacts
-
 
